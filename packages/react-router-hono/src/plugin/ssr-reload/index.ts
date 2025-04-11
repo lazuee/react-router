8d776fe89148cd175350c02fba4237539054819b
John Marlo Lapiz <lazuee.dev@gmail.com>
2025-04-11 16:57:53 +0000

    update

--- packages/react-router-hono/src/plugin/ssr-reload/index.ts
+++ packages/react-router-hono/src/plugin/ssr-reload/index.ts
@@ -7,30 +7,35 @@ import { type EnvironmentModuleNode, type Plugin } from "vite";
 export function plugin(): Plugin {
   return {
     name: "@lazuee/react-router-hono[ssr-reload]",
-    hotUpdate({ modules, server, timestamp }) {
-      if (this.environment?.name !== "ssr") return;
+    enforce: "post",
+    hotUpdate: {
+      order: "post",
+      handler({ modules, server, timestamp }) {
+        if (this.environment?.name !== "ssr") return;
 
-      const seen = new Set<EnvironmentModuleNode>();
-      let hasSsrOnlyModule = false;
+        const seen = new Set<EnvironmentModuleNode>();
+        let hasSsrOnlyModules = false;
 
-      for (const mod of modules) {
-        const skip =
-          !mod.id ||
-          server.environments.client.moduleGraph.getModuleById(mod.id);
-        if (skip) continue;
+        for (const mod of modules) {
+          const skip =
+            !mod.id ||
+            server.environments.client.moduleGraph.getModuleById(mod.id);
+          if (skip) continue;
 
-        this.environment.moduleGraph.invalidateModule(
-          mod,
-          seen,
-          timestamp,
-          false,
-        );
-        hasSsrOnlyModule = true;
-      }
+          this.environment.moduleGraph.invalidateModule(
+            mod,
+            seen,
+            timestamp,
+            true,
+          );
+          hasSsrOnlyModules = true;
+        }
 
-      if (hasSsrOnlyModule || modules.length === 0) {
-        server.ws.send({ type: "full-reload" });
-      }
+        if (hasSsrOnlyModules) {
+          server.ws.send({ type: "full-reload" });
+          return [];
+        }
+      },
     },
   };
 }
