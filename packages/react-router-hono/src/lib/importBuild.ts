97a51f0346b88df7a4c93f341c7e0c1ac59f60ac
John Marlo Lapiz <lazuee.dev@gmail.com>
2025-05-04 16:06:30 +0000

    update
    - resolve loader hot-reload issue
    - bump dependencies version

--- packages/react-router-hono/src/lib/importBuild.ts
+++ packages/react-router-hono/src/lib/importBuild.ts
@@ -1,21 +1,19 @@
 import { type ServerBuild } from "react-router";
 
-let _build: ServerBuild;
-
 export async function importBuild(): Promise<ServerBuild | undefined> {
   if (__reactRouterHono.mode !== "production") {
-    return (_build ??= (
+    return (
       await import("vite").then(({ createServer }) =>
         createServer({
           server: { middlewareMode: true, ws: false },
           appType: "custom",
         }),
       )
-    ).ssrLoadModule("virtual:react-router/server-build") as any);
+    ).ssrLoadModule("virtual:react-router/server-build") as any;
   }
 
-  return (_build ??= await import(
+  return await import(
     //@ts-expect-error - virtual module
     /* @vite-ignore */ "virtual:react-router/server-build"
-  ));
+  );
 }
